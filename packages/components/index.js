b1175a89506535dc810e7e7c7a33ee027251edd9
Yucheng Zhou <yucheng.zhou@zerologix.com>
2020-11-27 18:00:47 +1100

    test publish

--- packages/components/index.js
+++ packages/components/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 export const Section1 = () => {
     return (
         <div>
-            <h1>This is Section1</h1>
+            <h1>This is Section 1</h1>
         </div>
     )
 }
